ae10749e3a0ceba3d7cb0c1a7ca90d1202dad332
vlakreeh <zeb@zebulon.dev>
2021-01-23 08:54:35 -0500

    More documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,34 @@
+//! # A crate for trying things again.
+//! `tryagain` is a crate to try things again if they fail inspired by
+//! [backoff](https://docs.rs/backoff) that offers an easier way to cancel
+//! retry attemps and uses a non-blocking async implementation.
+//! ## Sync example
+//! ```
+//! # use tryagain::*;
+//! fn fails() -> Result<(), i32> {
+//! #   return Ok(()); // So our doctests pass.
+//!     Err(0)
+//! }
+//!
+//! // Will never resolve into, will spin forever.
+//! let value = tryagain::retry(ImmediateBackoff, fails);
+//! ```
+//! ## Async example
+//! ```
+//! # use tryagain::*;
+//! # async {
+//! async fn fails() -> Result<(), i32> {
+//! #   return Ok(()); // So our doctests pass.
+//!     Err(0)
+//! }
+//! 
+//! // Will never resolve into, will spin forever.
+//! let value = tryagain::future::retry(ImmediateBackoff, fails).await;
+//! # };
+//! ```
+
 #![forbid(unsafe_code)]
+#![forbid(missing_docs)]
 
 #[cfg(any(feature = "runtime-tokio", feature = "runtime-async-std"))]
 pub mod future;
